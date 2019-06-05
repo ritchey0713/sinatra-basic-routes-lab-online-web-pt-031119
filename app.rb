cbba30180fd74120aeb97740e359733e8a950e3f
David Ritchey <ritchey0713@gmail.com>
2019-06-05 19:07:52 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  
+  get "/name" do 
+    "My name is Bob"
+  end
+  
+  get "/hometown" do 
+    "My hometown is Columbus"
+  end 
+  
+  get "/favorite-song" do 
+    "My favorite song is Song1"
+  end 
+  
 end
